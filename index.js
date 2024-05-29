971d7e031ac128f54983ea5b4dd44a0d4bef20df
niraj070707 <karandeniraj28@gmail.com>
2024-05-29 23:41:52 +0530

    Connect Server with mongoDB and Create User model

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+const userRouter = require('./routes/userRoutes')
 
+dotenv.config();
+connectDB();
 const app = express();
+
+app.use('/user', userRouter);
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000 !!!');
-})
+}) 
