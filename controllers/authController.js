3f1fc371aeb91f8d92573351aabfac78a09fa6ca
niraj070707 <karandeniraj28@gmail.com>
2024-06-02 19:57:34 +0530

    Create Signin api route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,13 @@
 const User = require("../models/userModel");
 const bcryptjs = require('bcryptjs');
 const { errorHandler } = require("../utils/errorHandler");
+const jwt = require('jsonwebtoken');
 
 exports.signup = async (req, res, next)=>{
     const {username , email, password} = req.body;
 
     if(!username || !email || !password || username === '' || email === '' || password === ''){
-        return res.status(400).json({message : 'All fields are required !!'});
+        return next(errorHandler(400, 'All fields are required !!'));
     }
 
     try{
@@ -20,7 +21,25 @@ exports.signup = async (req, res, next)=>{
 
         return res.status(200).json({message : 'SignUp Successful'});
     }catch(err){
-        console.log(errorHandler(500, 'Invalid'));
-        next(errorHandler(500, 'Invalid'));
+        next(err);
+    }
+}
+
+exports.signin = async (req, res, next)=>{
+    const {email, password} = req.body;
+
+    try{
+        const validUser = await User.findOne({ email });
+        if(!validUser){return next(errorHandler(404, 'User not found !!'));}
+
+        const validPssword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPssword){return next(errorHandler(401, 'Wrong credentials !!'));}
+        
+        const {password : pass, ...userWithoutPassword} = validUser._doc;
+        console.log(userWithoutPassword);
+        const token = jwt.sign({id : validUser._id},process.env.SECRET_KEY);
+        res.cookie('token', token, {httpOnly : true}).status(200).json(userWithoutPassword);
+    }catch(err){
+        next(err); 
     }
 }
--- models/userModel.js
+++ models/userModel.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username : {
-        type : String,
-        unique : true,
-        require : true,
+    username: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    email : {
-        type : String,
-        unique : true,
-        require : true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    password : {
-        type : String,
-        require : true,
-    }
-}, {timestamps : true});
+    password: {
+        type: String,
+        required: true,
+    },
+}, {timestamps : true}); 
 
 const User = mongoose.model('User', userSchema);
 
